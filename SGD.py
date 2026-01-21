10173c3bdde3fdc32a160861771738ea08e0e116
chapa.Mahendra babu <mahindrachapa@gmail.com>
2026-01-21 10:10:09 +0530

    Add files via upload

---
+++ SGD.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDRegressor 
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.datasets import fetch_california_housing
+import numpy as np
+
+# Load data
+data = fetch_california_housing(as_frame=True)
+df = data.frame
+
+X = df.drop('MedHouseVal', axis=1)
+y = df['MedHouseVal']
+
+# Column separation
+cat_cols = X.select_dtypes(include=['object', 'category']).columns
+num_cols = X.select_dtypes(include=['int64', 'float64']).columns
+
+# Preprocessing
+preprocessing = ColumnTransformer(
+    transformers=[
+        ('num', Pipeline([
+            ('impute', SimpleImputer(strategy='mean')),
+            ('scaler', StandardScaler())
+        ]), num_cols)
+    ]
+)
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Linear Regression Pipeline
+model = Pipeline([
+    ('preprocessing', preprocessing),
+    ('regressor', SGDRegressor(loss="squared_error",penalty="l1",alpha=0.1))
+])
+
+# Train
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Evaluation
+print("R²:", r2_score(y_test, y_pred))
+print("MAE:", mean_absolute_error(y_test, y_pred))
+print("RMSE:", mean_squared_error(y_test, y_pred))
